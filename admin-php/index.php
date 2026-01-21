dbdaf1a9ca85a9915e29faf10f7a213bae04fd43
Quek Elijah <50226520+elijahquek@users.noreply.github.com>
2026-01-21 17:58:21 +0800

    Added PHP

--- admin-php/index.php
+++ admin-php/index.php
@@ -1,5 +1,7 @@
 <form action="save_config.php" method="POST">
-  Cooldown Time:
-  <input type="number" name="cooldown" step="0.1">
+  Cooldown:
+  <input name="cooldown_time" type="number" step="0.1">
+  Stamina Cost:
+  <input name="stamina_cost" type="number">
   <button type="submit">Save</button>
 </form>
